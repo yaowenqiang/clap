2a1b2f95ecfdebe6368d4507a364675993b7c128
yaowenqiang <yaowenqiang111@163.com>
2020-05-11 00:33:08 +0800

    add maud static template macors demo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+#![feature(proc_macro_hygiene)]
+
 use clap::{clap_app, crate_version};
 use pulldown_cmark::{html::push_html, Event, Parser};
+use maud::html;
 
 fn main() {
     let clap = clap_app!(mdrend => 
@@ -7,21 +10,49 @@ fn main() {
                      (author: "Jacky.yao")
                      (about: "Renders markdown as you like")
                      (@arg input: +required "Sets the input file")
+                     (@arg wrap: -w "Wrap in html")
+                     (@arg event: -e "Print events")
+                     (@arg css: --css +takes_value  "Link to css")
      )
     .get_matches();
     println!("Input = : {:?}", clap.value_of("input"));
     let infile = std::fs::read_to_string(clap.value_of("input").unwrap()).expect("Could not read file");
     let ps = Parser::new(&infile);
     let mut res = String::new();
-
     let ps: Vec<Event> = ps.into_iter().collect();
-
-    for p in &ps {
-        println!("{:?}", p);
+    if clap.is_present("event") {
+        for p in &ps {
+            println!("{:?}", p);
+        }
     }
+
     push_html(&mut res, ps.into_iter());
+
+    if clap.is_present("wrap") {
+        res = wrap_html(&res, clap.value_of("css"));
+    }
 
     println!("{}", res);
 
     println!("done");
+
+
+}
+
+fn wrap_html(s:&str, css:Option<&str>) -> String {
+    let res = html!{
+        (maud::DOCTYPE)
+        html {
+            head {
+                meta charset="utf-8";
+                @if let Some(s) = css {
+                    link rel="stylesheet" type="text/css" href=(s)  {}
+                }
+            }
+            body {
+                (maud::PreEscaped(s))
+            }
+        }
+    };
+    res.into_string()
 }
